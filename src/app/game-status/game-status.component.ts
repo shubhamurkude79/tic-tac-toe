cd4c095a980230de834f01231e71a29a47f3223d
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-10-15 23:47:51 +0530

    game status page created

--- src/app/game-status/game-status.component.ts
+++ src/app/game-status/game-status.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-status',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-status.component.html',
   styleUrl: './game-status.component.scss'
 })
 export class GameStatusComponent {
-
+  @Input() currentPlayer!: string;
+  @Input() winner!: string | null;
 }
